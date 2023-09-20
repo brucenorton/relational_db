a76768a931bc050262423835a0ccefbe9ed27ab3
Bruce Norton <nortonb@vaniercollege.qc.ca>
2023-09-20 09:20:23 -0400

    updated... insert new users

--- app/bcrypt.php
+++ app/bcrypt.php
@@ -2,9 +2,14 @@
 // a quick test to verify a password
 //create an encrypted password here: 
 // https://www.devglan.com/online-tools/bcrypt-hash-generator
-$hash = '$2a$04$lHRmTSExsly7YoklX.0j6eEEPJzDPDBp2Rac8idEsYz2uBMY/fr1S';
+//$hash = '$2a$04$lHRmTSExsly7YoklX.0j6eEEPJzDPDBp2Rac8idEsYz2uBMY/fr1S';
+$options = [
+    'cost' => 12,
+];
+$hash = password_hash('hvrtHunti9', PASSWORD_DEFAULT, $options);
 
 if (password_verify('hvrtHunti9', $hash)) {
+    echo($hash);
     echo 'Password is valid!';
 } else {
     echo 'Invalid password.';
--- app/login.php
+++ app/login.php
@@ -18,18 +18,13 @@
       $results[] = $row;
     }
     if (!mysqli_num_rows($result)) {
+      $results[]= ['verify'=>true];
+      return $results;
       throw new Exception('user_does_not_exist' . mysqli_stmt_error($stmt));
     }else{
       if(password_verify($_REQUEST['password'], $results[0]['password'])){
         $results[]= ['verify'=>true];
-
-        $_SESSION['verify'] = true;
-        $_SESSION['userID'] = $results[0]['userID'];
-        $_SESSION['full_name'] = $results[0]['full_name'];
-        $_SESSION['email'] = $results[0]['email'];
-
-        //Boolean switch type variable
-        $_SESSION['logged_in'] = true;
+        $results[]= ['updated'=>'20/09/2023'];//just for testing... can be deleted
         return $results;
         
       }else{
@@ -40,6 +35,48 @@
     }
   }
 
+  //updated 20/09/2023
+  //abstracted out session info
+  function setSession($results){
+    
+    $_SESSION['verify'] = true;
+    $_SESSION['userID'] = $results[0]['userID'];
+    $_SESSION['full_name'] = $results[0]['full_name'];
+    $_SESSION['email'] = $results[0]['email'];
+
+    //Boolean switch type variable
+    $_SESSION['logged_in'] = true;
+  }
+
+  function insertNewUser($link){
+    $query = "INSERT INTO users (email, password) VALUES (?, ?)";
+
+    if($stmt = mysqli_prepare($link, $query)){
+      //cost slows server, but makes it more secure.
+      $options = [
+        'cost' => 12,
+      ];
+      $password = password_hash($_REQUEST["password"], PASSWORD_DEFAULT, $options);
+      mysqli_stmt_bind_param($stmt, "ss", $_REQUEST["email"], $password);
+      mysqli_stmt_execute($stmt);
+      $insertedRows = mysqli_stmt_affected_rows($stmt);
+
+      if($insertedRows > 0){
+        $results[] = [
+          "insertedRows"=>$insertedRows,
+          "id" => $link->insert_id,
+          "email" => $_REQUEST["email"],
+          "verify"=> true
+        ];
+        setSession($results);
+      }else{
+        throw new Exception("No rows were inserted");
+      }
+      return $results;
+    }
+  }
+
+
  //main logic of the application is in this try{} block of code.
   try{
    
@@ -49,10 +86,11 @@
     }else{
       $results = selectUser($link);
       if($results[1]['verify']){
-        //store session info
-        //insert new favourite
+        //abstract out adding session info?
+        setSession($results);
       }else{
-        //add new user??
+        //if $results[1]['verify']=false
+        $results[] = insertNewUser($link);
       }
       
     }
