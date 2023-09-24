2030cee8f1c66dc4a8d931308ef530aaa6d504c8
Bruce Norton <nortonb@vaniercollege.qc.ca>
2023-09-24 15:44:52 -0400

    added tarantino

---
+++ app/tarantino.php
@@ -0,0 +1,30 @@
+<?php
+  //connect to db - $link
+  require_once "_includes/db_connect.php";
+
+  //prepare the statement passing the db $link and the SQL
+  /* ADDED "'ORDER BY' timestamp DESC" at the end of the query for reverse, chronological order */
+  $stmt = mysqli_prepare($link, 
+  "SELECT *
+  FROM tarantino_movies, tarantino_actors
+  WHERE tarantino_movies.actorID = tarantino_actors.actorID
+  ");
+
+  //execute the statement / query from abobe
+  mysqli_stmt_execute($stmt);
+
+  //get results
+  $result = mysqli_stmt_get_result($stmt);
+
+  //loop through
+  while($row = mysqli_fetch_assoc($result)){
+    $results[] = $row;
+  }
+
+  //encode & display json
+  echo json_encode($results);
+
+  //close the link to the db
+  mysqli_close($link);
+
+?>
